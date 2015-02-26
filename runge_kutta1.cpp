b1c538a113e88174c054a6540db665a27cd8e2ac
Andy Perez <Ichiroska@gmail.com>
2015-02-26 10:55:16 -0500

    Added a test for the adaptive Runge-Kutta I'm about to code

--- runge_kutta1.cpp
+++ runge_kutta1.cpp
@@ -1,17 +1,14 @@
-/* Basic adaptive Runge-Kutta ODE algorithm implementation along with example */
-
-#include<iostream>
-
-/* Program solves differential equations of the form dx/dt = F(x(t),t)
- * the function F(x(t), t) should have the following signature: */
-
-double functionalPrototypeF(double, double);
+/* Basic adaptive Runge-Kutta ODE algorithm implementation
+ * Program solves differential equations of the form dx/dt = F(x(t),t)
+ */
 
 /* Given an initial step size h0, desired accuracy d, and an initial x0,t0, 
  * (all doubles), as well as the funcion F(x(t), t) of the form shown above,
  * we will calculate k1, k2, k3, k4, and then finally x1, the next value of x */
 
-double exx1(double h, double x, double t, double (*F)(double, double) ){
+#include<functional>
+
+double exx1(double h, double x, double t, std::function<double(double, double)> F){
 	double kay1 = h*F(x,t);
 	double kay2 = h*F(x + .5*kay1, t + .5*h);
 	double kay3 = h*F(x + .5*kay2, t + .5*h);
--- runge_kutta1_test.cpp
+++ runge_kutta1_test.cpp
@@ -4,7 +4,7 @@
 //
 #include<iostream>
 #include<cmath>
-#include"runge-kutta1.cpp"
+#include"runge_kutta1.cpp"
 #include<vector>
 #include<iomanip>
 
@@ -12,10 +12,56 @@
 #include "TCanvas.h"
 #include "TROOT.h"
 #include "TF1.h"
-#include "TApplication.h"
+//#include "TApplication.h"
 #include "TGraph.h"
+#include "TMultiGraph.h"
 
 // Compile with g++ and additional option 'root-config --cflags --glibs'
+
+void double_rungeKutta1_test_1() {
+	/* Let's try this on a few functions.
+	 * (d/dt)sin(3t) = 3cos(3t) = F(x,t) 
+	 * (d/dt)cos(2t) = -2sin(2t) = F(x,t)
+	 * Both with desired error e < 10^-4, on the
+	 * interval [-5,5] (tested at endpoint and midpoint)
+	 */
+
+	std::vector<double> kutta_t;
+	std::vector<double> kutta_x;
+	const double tBounds[2] = {-5., 5};
+	const double e = .0001;
+
+	// Defining the two Fs and the intial values for the two testing functions
+	std::function<double(double)> F1([](double){return 3.*std::cos(3*t);});
+	const double x01 = std::sin(3*tBounds[0]);
+	std::function<double(double)> F2([](double){return -2.*std::sin(2*t);});
+	const double x02 = std::cos(2*tBounds[0]);
+
+	TCanvas* canvas = new TCanvas();
+	TMultiGraph* mg = new TMultiGraph();
+	mg->setTitle("Runge-kutta approximation of sin(3t), cos(2t)");
+	if (rungeKutta1(F1 , x01, &kutta_t, &kutta_x, tBounds[0], tBounds[1], e);) {
+		std::cout << "Runge-kutta sin(3t) Successful!" << std::endl;
+		TGraph* rungeGraph(kutta_t.size(), kutta_t.data(), kutta_x.data());
+		mg->Add(rungeGraph);
+	} else {
+		std::cout << "Runge-kutta sin(3t) Failed" << std::endl;
+	} 
+	// Empty out the vectors first of old data
+	kutta_t.clear(); kutta_x.clear();
+
+	if (rungeKutta1(F2 , x02, &kutta_t, &kutta_x, tBounds[0], tBounds[1], e);) {
+		std::cout << "Runge-kutta cos(2t) Successful!" << std::endl;
+		TGraph* rungeGraph2(kutta_t.size(), kutta_t.data(), kutta_x.data());
+		mg->Add(rungeGraph2);
+	} else {
+		std::cout << "Runge-kutta cos(2t) Failed" << std::endl;
+	}
+	return;
+}
+
+
+
 
 void double_exx1_test_1(){
 	/* Let's first test this on sin(t)
@@ -26,7 +72,7 @@ void double_exx1_test_1(){
 	std::vector<double> kutta_t;
 	std::vector<double> kutta_x;
 
-	const float tBounds[2] = {-5.,5.};
+	const float tBounds[2] = {0,100};
 
 	auto cosine = [](double x, double t)->double { return std::cos(t); };
 	double(*F)(double, double) = cosine;
@@ -53,23 +99,49 @@ void double_exx1_test_1(){
 	// Drawing an actual cosine
 	TF1* sine = new TF1("Fsine", "sin(x)", tBounds[0], tBounds[1]);
 	sine->SetLineColor(kRed);
+	sine->SetNpx(1000);
 	sine->DrawClone("Same");	
 
 	return;
 }
 
+void double_exx1_test_3() {
+	std::vector<double> kutta_t;
+	std::vector<double> kutta_x;
+
+	const double tBounds[2] = {-5.,5.};
+
+	auto poly = [](double x, double t)->double{return t*t*3.;};
+	double(*F)(double, double) = poly;
+	const double h0 = .1;
+	double x = -125.;
+	for(double t = tBounds[0]; t < tBounds[1] ; t += h0) {
+		kutta_t.push_back(t);
+		kutta_x.push_back(x);
+		x = exx1(h0, x, t, F);
+	}
+	TGraph rungeGraph(int(kutta_t.size()), kutta_t.data(), kutta_x.data());
+
+	TCanvas* canvas = new TCanvas();
+
+	rungeGraph.SetTitle("Runge-Kutta approximation of t^3");
+	rungeGraph.SetLineColor(kBlue);
+	rungeGraph.DrawClone("AL");
+	std::cout << "At four, we have " << rungeGraph.Eval(4,0,"");
+	return;
+}
 
 void double_exx1_test_2(){
 	/* Let's try it on the exponential function now!*/
 	std::vector<double> kutta_t;
 	std::vector<double> kutta_x;
 
-	const float tBounds[2] = {-5.,20.};
+	const float tBounds[2] = {-5.,30.};
 
 	auto exponential = [](double x, double t)->double { return std::exp(t); };
 	double(*F)(double, double) = exponential;
 
-	const double h0 = 1;
+	const double h0 = .1;
 
 	double x = std::exp(tBounds[0]);
 	for(double t = tBounds[0]; t < tBounds[1] ; t += h0) {
@@ -108,5 +180,11 @@ void runge_kutta1_test() {
 	std::cin >> yesno;
 	if (yesno=='y') {
 		double_exx1_test_2();
+	}
+	std::cout << std::endl;
+	std::cout << "Run test 3? (y/n): ";
+	std::cin >> yesno;
+	if (yesno=='y') {
+		double_exx1_test_3();
 	}
 }
